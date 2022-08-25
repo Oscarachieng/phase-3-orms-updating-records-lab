ef3fcc055580bb286c42d972e3f8800c6c615f93
Oscar Abuto Achieng <oscarachieng94@gmail.com>
2022-08-25 10:38:31 +0300

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -5,7 +5,9 @@ class Student
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]
   #initialize method
-  attr_accessor :name, :grade, :id
+  attr_accessor :name, :grade
+  attr_reader :id
+  
   def initialize (name, grade, id = nil)
     @id = id
     @name = name
